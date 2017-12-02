19fa38a5680d3d5212f1680ed6bb2cc5315e0458
Relton <ori.taylor@gmail.com>
2017-12-02 22:35:05 +0000

    Initialising Project
    +Added main.lua and conf.lua
    +Added resolution in conf.lua (may be changed) as well as title and Love version
    +Added initialisations in main.lua

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+
+	t.title = "Library Test"
+	t.window.width = 1200
+	t.window.height = 750
+	t.version = "0.10.1"
+	
+end
---
+++ main.lua
@@ -0,0 +1,19 @@
+--Requires
+
+
+--Loads
+function love.load(){
+	--N/A
+}
+
+--Updates
+function love.update(){
+	--N/A
+}
+
+--Drawing
+function love.draw(){
+	--N/A
+}
+
+--NOTE TO ANYONE USING THIS LIBRARY: main.lua is un-needed as none of the functions are stored here. Only use this file for experimentations with the library.
